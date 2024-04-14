0009e13744c1af13ae22ea4b713cea8feae25aa2
amirrezaflh1402 <amirrezaflh1402@gmail.com>
2024-04-14 09:31:52 +0330

    env

--- env.sh
+++ env.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 # Define the environment variable prefix
-ENV_PREFIX="FLICKIT_LANDING"
-
-# Loop through environment variables
-for i in $(env | grep "^$ENV_PREFIX")
+for i in $(env | grep FLICKIT_)
 do
     key=$(echo $i | cut -d '=' -f 1)
     value=$(echo $i | cut -d '=' -f 2-)
+    echo $key=$value
     
     # Replace occurrences of the environment variable in files
     find .next -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +

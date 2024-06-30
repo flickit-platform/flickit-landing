4942b56f6976dc4daf4b70bf334e491d81c6e9f1
mostafa.esmaeili <e.mostafa021@gmail.com>
2024-06-30 15:49:44 +0330

    FL-7: top main fix

--- src/components/homePage/topHeader.tsx
+++ src/components/homePage/topHeader.tsx
@@ -7,12 +7,14 @@ export default function TopHeader(props:{ redirectUrl: string }) {
 
     return (
         <main className="mx-auto flex justify-center items-center relative h-[28.3rem] w-full  sm:max-h-[45.3rem] sm:h-[45.3rem]  lg:max-h-[62.3rem] lg:h-[62.3rem] ">
-            <img src={"/lines.svg"} className={"absolute h-full w-full top-0 bottom-0 right-0 left-0"} />
-                <section className={`${sansation.className } text-center gap-y-4 sm:gap-y-5 flex flex-col items-center justify-center w-[18.5rem] h-[20rem] sm:w-[32rem] sm:h-[30rem] lg:w-[55.43rem] 2xl:w-[60rem] 3xl:w-[80rem]`} >
+            <div  className={"absolute bg-cover bg-[url(/lines.svg)] h-full w-full top-0 bottom-0 right-0 left-0"} />
+            <div className={"flex justify-center items-center w-full h-[20rem]  sm:h-[30rem]"}>
+                <div className={"absolute bg-cover blur-2xl bg-[url(/meshGradiant.svg)] h-full w-full top-0 bottom-0 right-0 left-0"}/>
+                <section className={`${sansation.className} relative  text-center gap-y-4 sm:gap-y-5 flex flex-col items-center justify-center w-[18.5rem] sm:w-[32rem] lg:w-[55.43rem] 2xl:w-[60rem] 3xl:w-[80rem]`}>
                     <div className="text-[0.688rem] sm:text-[1.563rem] text-[#EDF4FC] font-normal leading-8">
                         The ultimate tool for assessing your software
                     </div>
-                    <div className=" text-[#EDF4FC] text-[1.5rem] sm:text-[2.5rem] lg:text-[4.5rem] font-bold leading-8 sm:leading-10 lg:leading-[5.625rem]">
+                    <div className="drop-shadow-[5px_5px_20px_rgba(255,255,255,0.4)] text-[#EDF4FC] text-[1.5rem] sm:text-[2.5rem] lg:text-[4.5rem] font-bold leading-8 sm:leading-10 lg:leading-[5.625rem]">
                         Say Goodbye to Software Quality Blind Spots with Expert Algorithms
                     </div>
                     <div className="font-light sm:font-normal text-[0.75rem] sm:text-[1.25rem] text-[#EDF4FC]">
@@ -30,6 +32,7 @@ export default function TopHeader(props:{ redirectUrl: string }) {
                     </div>
                 </section>
 
+            </div>
         </main>
     )
 }

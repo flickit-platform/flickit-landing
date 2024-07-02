3da3a659f4ebab72f8ffa3e2147e041cac8f8eca
mostafa.esmaeili <e.mostafa021@gmail.com>
2024-07-02 14:53:23 +0330

    FL-7: flickit is helpful v3

--- src/components/homePage/flickitIsHelpful.tsx
+++ src/components/homePage/flickitIsHelpful.tsx
@@ -4,10 +4,10 @@ export default function FlickitIsHelpful() {
     return (
         <main className="mx-auto flex justify-center items-center py-[2.37rem]  ">
             <section
-                className=" flex flex-col justify-start items-center rounded-2xl pt-7 pb-12 gap-9 max-w-[80rem] w-[95%] bg-[#3B4F68] bg-opacity-30 relative">
-                <div className="absolute top-0 right-0 left-0 bottom-0 bg-[url(/bgShapes.svg)] blur-lg"/>
+                className=" flex flex-col justify-start items-center rounded-2xl pt-7 pb-12 sm:pt-20 gap-9 sm:gap-16 lg:gap-24 max-w-[90rem] w-[95%] bg-[#3B4F68] bg-opacity-30 relative">
+                <div className="absolute top-0 right-0 left-0 bottom-0 bg-[url(/bgShapes.svg)] bg-no-repeat bg-cover blur-xl"/>
                 <div className="absolute top-0 right-0 left-0 bottom-0 bg-[url(/vector.svg)] mix-blend-overlay"/>
-                <h2 className="text-white mx-auto font-medium text-[1.37rem]">
+                <h2 className="text-white mx-auto font-medium sm:font-bold text-[1.37rem] sm:text-[2.2rem]  lg:text-[4rem] ">
                     Flickit is specially helpful if...
                 </h2>
                 {flickitIsHelpful.map((item, index)=>{
@@ -15,11 +15,10 @@ export default function FlickitIsHelpful() {
                         <section
                             key={index}
                             style={{background: "radial-gradient(circle at center, rgba(108, 123, 142, 40%) 0%, rgba(59, 79, 104, 40%) 100%)"}}
-                            className={"rounded-lg px-5 pt-7 pb-4 justify-center items-center gap-y-3 sm:gap-[3.25rem] relative w-[90%] min-h-[14rem] grid grid-cols-1 sm:grid-cols-2 col-span-6"}>
-                            <div
-                            style={{background: `url(${item.img})`,backgroundRepeat: "no-repeat"}}    className="absolute flex justify-self-center w-[50px] h-[50px] -top-[20px]"/>
-                            <h3 className="text-white text-center sm:text-right text-sm font-medium">{item.title}</h3>
-                            <p className="text-xs text-[#EFF1F3] sm:text-left">
+                            className={"rounded-lg px-5 pt-7 pb-4 justify-center items-center gap-y-3  sm:gap-x-[3.25rem] relative w-[90%] sm:min-h-[14rem] grid grid-cols-1 sm:grid-cols-2 col-span-6"}>
+                            <div style={{background:`url(${item.img})`,backgroundRepeat: "no-repeat",backgroundPosition: "center top",backgroundSize:"contain"}} className="absolute flex justify-self-center w-[36px] h-[36px] sm:w-[70px] sm:h-[70px] -top-[18px] sm:-top-[33px]"/>
+                            <h3 className="text-white text-center sm:text-right text-sm font-medium sm:font-semibold sm:text-xl lg:text-3xl  ">{item.title}</h3>
+                            <p className="text-xs text-[#EFF1F3] sm:text-left sm:text-sm lg:text-base">
                             {item.text}
                             </p>
                         </section>
--- src/components/homePage/topMain.tsx
+++ src/components/homePage/topMain.tsx
@@ -3,18 +3,20 @@ import {cardsText} from "@/utils/db";
 export default function TopMain() {
 
     return (
-        <section className="flex flex-col items-center gap-6 bg-gradient-to-b from-0% from-bluemaxLight to-100% to-bluemaxDeep p-9 ">
-            <h3 className="text-white font-semibold text-[1.5rem] leading-8">
-                How does it work?
-            </h3>
-            <article className="flex flex-col sm:flex-row justify-center items-stretch gap-4 lg:w-[75%]">
-                {cardsText.map((card,index) =>{
-                    const {title, text, color} = card
-                    return (
-                        <Cards title={title} text={text} color={color} key={index} />
-                    )
-                })}
-            </article>
+        <section className="flex items-center justify-center bg-gradient-to-b from-0% from-bluemaxLight to-100% to-bluemaxDeep p-9 ">
+            <div className="flex flex-col items-center justify-center gap-6 max-w-[90rem]">
+                <h3 className="text-white font-semibold text-[1.5rem] leading-8">
+                    How does it work?
+                </h3>
+                <article className="flex flex-col sm:flex-row justify-center items-stretch gap-4 lg:w-[75%]">
+                    {cardsText.map((card, index) => {
+                        const {title, text, color} = card
+                        return (
+                            <Cards title={title} text={text} color={color} key={index}/>
+                        )
+                    })}
+                </article>
+            </div>
         </section>
     )
 
@@ -22,7 +24,8 @@ export default function TopMain() {
         return (
             <article
                 className="flex flex-col  justify-start lg:justify-between  gap-y-3 w-full  px-4 py-3 sm:px-[1.8rem] sm:py-[0.6rem] lg:px-[2.8rem] lg:py-[1.6rem] rounded-2xl sm:rounded-[2rem] bg-[#375172] bg-opacity-30 sm:min-h-[16.6rem] sm:max-h-[16.6rem]">
-                <h2 style={{color}} className={`text-base sm:text-[1.1rem] lg:text-[1.5rem] font-semibold text-left lg:font-bold sm:text-center colorText h-1/3`}>
+                <h2 style={{color}}
+                    className={`text-base sm:text-[1.1rem] lg:text-[1.5rem] font-semibold text-left lg:font-bold sm:text-center colorText h-1/3`}>
                     {title}
                 </h2>
                 <p className="text-xs sm:text-[0.8rem] text-whiteGray font-light leading-relaxed tracking-normal text-justify text-overFlow5 flex justify-center items-center  h-2/3">

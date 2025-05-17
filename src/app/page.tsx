6ab230ec075b1e61b1668e5baf7287dadce91d85
e.mostafa <e.mostafa021@gmail.com>
2025-05-17 13:14:55 +0330

    FRO-403: feat : new landing config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import {useEffect, useState} from "react";
-import {getRedirectUrl} from "@/utils/constants";
 import Footer from "@/components/homePage/footer";
 import "../config/i18n";
 import HeroSection from "@/components/homePage/heroSection";
 import Cards from "@/components/homePage/cards";
 
 export default function Home() {
-    const [setRedirectUrl] = useState('');
 
-    useEffect(() => {
-        setRedirectUrl(getRedirectUrl());
-    }, []);
 
     return (
         <>
--- src/components/nav/FINav.tsx
+++ src/components/nav/FINav.tsx
@@ -1,16 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
-import { getRedirectUrl } from "@/utils/constants";
-
 
 export default function FINav() {
-  const [setRedirectUrl] = useState("");
-
-  useEffect(() => {
-    setRedirectUrl(getRedirectUrl());
-  }, []);
-
-
 
   return (
     <nav>

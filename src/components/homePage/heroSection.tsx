d57bf3dc998b06165a34a7b0e40f4ba8bc368e04
e.mostafa <e.mostafa021@gmail.com>
2025-05-18 19:45:28 +0330

    FRO-404: fix : fix heroSection.tsx with new img

--- src/components/homePage/heroSection.tsx
+++ src/components/homePage/heroSection.tsx
@@ -3,7 +3,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import {Trans} from "react-i18next";
 import {theme} from "@/config/theme";
-import {useMediaQuery} from "@mui/material";
+import {Button, useMediaQuery} from "@mui/material";
 
 const HeroSection = () => {
 
@@ -11,66 +11,88 @@ const HeroSection = () => {
 
     return (
         <Box
-            sx={{
-                width: "100%",
-                height: "auto",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundImage: {
-                    xs: "url(/heroBg-mobile.svg)",
-                    sm: "url(/heroBg-desktop.svg)"
-                },
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat !important",
-                backgroundPosition: "center"
-            }}
+        sx={{display: "flex" ,height: "auto"}}
         >
-
+            <Box sx={{height: "auto", flex: 1, bgcolor: "#1B4D7E"}}/>
             <Box
                 sx={{
-                    // maxWidth: "1440px",
-                    mx: {xs: "52px", sm: "auto"},
-                    maxWidth: {xs: "336px", sm: "657px"},
-                    color: "#fff",
-                    textAlign: "center"
+                    width: "100%",
+                    maxWidth: "1533px",
+                    height: "auto",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundImage: {
+                        xs: "url(/flickitHeroBg-m.svg)",
+                        sm: "url(/flickitHeroBg-d.svg)"
+                    },
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat !important",
+                    backgroundPosition: "center"
                 }}
             >
-                <Typography sx={{
-                    ...theme.typography.displayLarge,
-                    [theme.breakpoints.down('sm')]: {
-                        ...theme.typography.headlineMedium,
-                        fontSize: '2.25rem',
-                        fontWeight: "bolder",
-                        lineHeight: "150%",
-                        letterSpacing: "-2%"
-                    },
-                    pt: {xs: "45px", sm: "77px"},
-                }}>
-                    <Trans i18nKey={"questionsAskedAnswers"}/>
-                </Typography>
+                <Box
+                    sx={{
+                        // maxWidth: "1440px",
+                        mx: {xs: "52px", sm: "auto"},
+                        maxWidth: {xs: "336px", sm: "657px"},
+                        color: "#fff",
+                        textAlign: "center"
+                    }}
+                >
+                    <Typography sx={{
+                        ...theme.typography.displayLarge,
+                        [theme.breakpoints.down('sm')]: {
+                            ...theme.typography.headlineMedium,
+                            fontSize: '2.25rem',
+                            fontWeight: "bolder",
+                            lineHeight: "150%",
+                            letterSpacing: "-2%"
+                        },
+                        pt: {xs: "45px", sm: "77px"},
+                    }}>
+                        <Trans i18nKey={"questionsAskedAnswers"}/>
+                    </Typography>
 
-                <Typography sx={{
-                    ...theme.typography.headlineSmall,
-                    lineHeight: "32px",
-                    fontSize: "2rem",
-                    fontWeight: "regular",
-                    [theme.breakpoints.down('sm')]: {
-                        ...theme.typography.semiBoldLarge,
-                        lineHeight: "24px"
-                    },
-                    mt: "20px"
-                }}>
-                    <Trans i18nKey={"topBannerDesc"}/>
-                </Typography>
-                <Box sx={{width: "100%", mt: {xs: "67px"}, mb: {xs: "76px"}, mx: "auto"}}>
-                    <Box
-                        component={"img"}
-                        src={isMobile ? "flickitMainIcon-m.svg" : "flickitMainIcon-d.svg"}
-                    />
+                    <Typography sx={{
+                        ...theme.typography.headlineSmall,
+                        lineHeight: "32px",
+                        fontSize: "2rem",
+                        fontWeight: "regular",
+                        [theme.breakpoints.down('sm')]: {
+                            ...theme.typography.semiBoldLarge,
+                            lineHeight: "24px"
+                        },
+                        mt: "20px"
+                    }}>
+                        <Trans i18nKey={"topBannerDesc"}/>
+                    </Typography>
+                    <Button
+                        variant={"contained"}
+                        sx={{bgcolor: "#F3F5F6",
+                            borderRadius: "100px",
+                            mt: {xs: "39px", sm:"27px"},
+                            mb:{xs: "169px", xl :"175px"},
+                            boxShadow: "0 1px 5px 0 #000"
+                        }}
+                    >
+                        <Typography sx={{
+                            ...theme.typography.headlineSmall,
+                            whiteSpace: "nowrap",
+                            [theme.breakpoints.down('sm')]: {
+                                ...theme.typography.semiBoldMedium,
+                            },
+                            color: "#2466A8"
+                        }}>
+                            <Trans i18nKey={"startSelfAssessment"} />
+                        </Typography>
+                    </Button>
                 </Box>
             </Box>
+            <Box sx={{height: "auto", flex: 1, bgcolor: "#D5E5F6"}}/>
         </Box>
+
+
     )
 };
 

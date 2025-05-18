d3de52cf9c7e123202a3b808a0c1f719e27c3d62
e.mostafa <e.mostafa021@gmail.com>
2025-05-18 11:50:06 +0330

    FRO-404: fix : change footer style

--- src/components/homePage/footer.tsx
+++ src/components/homePage/footer.tsx
@@ -3,41 +3,93 @@ import Typography from "@mui/material/Typography";
 import {Trans} from "react-i18next";
 import {Button} from "@mui/material";
 import Grid from '@mui/material/Grid';
+import {theme} from "@/config/theme";
+import {min} from "@popperjs/core/lib/utils/math";
+
 
 export default function Footer() {
+    const footerLinks = ["flickitApp", "assessmentKits", "blog"]
     return (
         <Box
             component={"footer"}
-            style={{background: "#2B333B"}}>
-            <Grid container spacing={2}>
-                {/*<Grid item*/}
-                {/*      xs={12}*/}
-                {/*      sm={4}*/}
-                {/*>*/}
-                {/*    <Box*/}
-                {/*        sx={{display: "flex", flexDirection: {xs: "row", sm: "column"}, gap: 2}}*/}
-                {/*    >*/}
-                {/*        <img src={"emailIcon.svg"}/>*/}
-                {/*        <Box sx={{*/}
-                {/*            display: "flex",*/}
-                {/*            flexDirection: "column",*/}
-                {/*            justifyContent: "center",*/}
-                {/*            alignItems: "center",*/}
-                {/*            gap: 2*/}
-                {/*        }}>*/}
-                {/*            <Typography sx={{*/}
-                {/*                color: "#F0F2F4",*/}
+            sx={{background: "#2B333B", py: "42px", px: {xs: 1, sm: "48px"}}}>
+            <Grid sx={{display: "flex", gap: {xs: "101px", sm: "unset"}}} container>
+                <Grid size={{xs: 12, sm: 4}}>
+                    <img src={"logo.svg"}/>
+                    <Typography sx={{...theme.typography.bodyMedium, color: "#F0F2F4", maxWidth: "428px"}}>
+                        <Trans i18nKey={"flickitFooterText"}/>
+                    </Typography>
+                </Grid>
+                <Grid size={{xs: 12, sm: 4}}
+                      sx={{
+                          color: "#F0F2F4",
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "center",
+                          alignItems: "center",
 
-                {/*            }}>*/}
-                {/*                <Trans i18nKey={"feelFreeToReachUs"}/>*/}
-                {/*            </Typography>*/}
-                {/*            <Button variant={"contained"}>*/}
-                {/*                <Trans i18nKey={"contactUs"}/>*/}
-                {/*            </Button>*/}
-                {/*        </Box>*/}
-                {/*    </Box>*/}
-                {/*</Grid>*/}
+                      }}>
+
+                    <Box sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: {xs: "center", sm: "flex-start"},
+                        gap: 2
+
+                    }}>
+                        <Typography sx={{
+                            ...theme.typography.titleMedium,
+                            textAlign: {sx: "center", sm: "left"}
+                        }}>
+                            <Trans i18nKey={"links"}/>
+                        </Typography>
+                        <Box sx={{display: "flex", flexDirection: {xs: "row", sm: "column"},  gap: {xs: "48px", sm: "unset"},}}>
+                            {footerLinks.map(link => {
+                                return (
+                                    <Typography key={link} sx={{...theme.typography.semiBoldMedium}}>
+                                        <Trans i18nKey={`${link}`}/>
+                                    </Typography>
+                                )
+                            })}
+                        </Box>
+
+                    </Box>
+                </Grid>
+                <Grid size={{xs: 12, sm: 4}}
+                >
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flexDirection: {xs: "row", sm: "column"},
+                            gap: 2
+                        }}
+                    >
+                        <img style={{width: "137px", height: "120px"}} src={"emailIcon.svg"}/>
+                        <Box sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            gap: 2
+                        }}>
+                            <Typography sx={{
+                                color: "#F0F2F4",
+
+                            }}>
+                                <Trans i18nKey={"feelFreeToReachUs"}/>
+                            </Typography>
+                            <Button variant={"contained"}>
+                                <Trans i18nKey={"contactUs"}/>
+                            </Button>
+                        </Box>
+                    </Box>
+                </Grid>
             </Grid>
         </Box>
     )
 }
+
+

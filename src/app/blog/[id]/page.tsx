ccf473e0ef47cdaad53eaf7edd332001e936eee5
e.mostafa <e.mostafa021@gmail.com>
2025-05-27 21:18:22 +0330

    FL-16: fix : add table

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -7,7 +7,13 @@ import Typography from "@mui/material/Typography";
 import {Trans} from "react-i18next";
 import {theme} from "@/config/theme";
 import {styles} from "@/config/styles";
-import {Divider} from "@mui/material";
+import {Divider, List, ListItem, ListItemText} from "@mui/material";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import data from "@/config/db/article.json"
 
 const mainBox = [
     {title: "Published on", subTitle: "Mar 4, 2020"},
@@ -21,6 +27,99 @@ const Article = (props: any) => {
         subTitle = "Facebook Messenger, with over a billion users worldwide, became an essential communication tool—but behind the scenes, it was struggling with a growing problem common in fast-evolving software: technical debt",
         img = "/articleTop.svg"
     } = props
+
+    const makeSentens = (item: any) => {
+        const {title, p, ul, table} = item
+
+        if (title) {
+            return <Typography sx={{...theme.typography.semiBoldXLarge, color: "#000", mb: 1}}>{title}</Typography>
+        } else if (p) {
+            return <Typography sx={{...theme.typography.semiBoldMedium, color: "#2B333B", mb: 2}}>{p}</Typography>
+        } else if (ul) {
+            return <List  disablePadding sx={{listStyleType: 'disc', paddingInlineStart: 3, mb: 2 }}>
+                {ul.map((itemText: any)=>{
+                    const { li } = itemText
+                    return (
+                        <ListItem  sx={{display: 'list-item'}} disablePadding>
+                            <ListItemText primaryTypographyProps={{sx:{ all:"unset", ...theme.typography.semiBoldMedium, color: "#2B333B"}}}  >{li}</ListItemText>
+                        </ListItem>
+                    )
+                })}
+            </List>
+
+        }else if(table){
+
+            return (
+              <>
+                    {table.map((tab: any)=>{
+                        return (
+                            <Table size="small" aria-label="a dense table">
+                            <TableHead>
+                                <TableRow>
+                                    {tab?.thead?.tr.map((i: any)=>{
+                                        const {th} = i
+                                        return (
+                                            <TableCell key={th}>{th}</TableCell>
+                                        )
+                                    })}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {tab?.tbody?.map((i: any)=>{
+
+                                    const {tr} = i
+                                    return (
+                                        <TableRow
+
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            {tr.map((item: any) =>{
+                                                const {td} = item
+                                                return (
+                                                    <TableCell align="left">{td}</TableCell>
+                                                )
+                                            })}
+                                        </TableRow>
+                                    )
+                                })}
+                            </TableBody>
+                            </Table>
+                        )
+                    })}
+              </>
+            )
+
+
+
+            // return <Table size="small" aria-label="a dense table">
+            //     <TableHead>
+            //         <TableRow>
+            //             {table.thead.tr.map(i=>{
+            //                 return (
+            //                     <TableCell>{i}</TableCell>
+            //                 )
+            //             })}
+            //         </TableRow>
+            //     </TableHead>
+            //     <TableBody>
+            //         {rows.map((row) => (
+            //             <TableRow
+            //                 key={row.name}
+            //                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            //             >
+            //                 <TableCell component="th" scope="row">
+            //                     {row.name}
+            //                 </TableCell>
+            //                 <TableCell align="right">{row.calories}</TableCell>
+            //                 <TableCell align="right">{row.fat}</TableCell>
+            //                 <TableCell align="right">{row.carbs}</TableCell>
+            //                 <TableCell align="right">{row.protein}</TableCell>
+            //             </TableRow>
+            //         ))}
+            //     </TableBody>
+            // </Table>
+        }
+    }
 
     return (
         <Box sx={{maxWidth: "824px", margin: "auto", mt: "40px"}}>
@@ -75,6 +174,11 @@ const Article = (props: any) => {
                     mb: 2
                 }}
             >
+            </Box>
+            <Box sx={{px: 13}}>
+                {data.facebookEn.content.map(item => {
+                    return makeSentens(item)
+                })}
             </Box>
         </Box>
     );

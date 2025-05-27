f0202b3b1fe100f881b49816cc71c9ec79a54555
e.mostafa <e.mostafa021@gmail.com>
2025-05-27 18:18:43 +0330

    FL-16: fix : fix comment

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -6,6 +6,8 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import {Trans} from "react-i18next";
 import {theme} from "@/config/theme";
+import {styles} from "@/config/styles";
+import {Divider} from "@mui/material";
 
 const mainBox = [
     {title: "Published on", subTitle: "Mar 4, 2020"},
@@ -17,7 +19,7 @@ const Article = (props: any) => {
     const {
         TopTitle = "How Facebook Messenger Cut Technical Debt to Boost Performance",
         subTitle = "Facebook Messenger, with over a billion users worldwide, became an essential communication tool—but behind the scenes, it was struggling with a growing problem common in fast-evolving software: technical debt",
-        img ="/articleTop.svg"
+        img = "/articleTop.svg"
     } = props
 
     return (
@@ -41,27 +43,38 @@ const Article = (props: any) => {
             }}>
                 {subTitle}
             </Typography>
-            <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", my: 2, py: 2}}>
+            <Box sx={{...styles.centerVH, my: 2, py: 2}}>
                 {mainBox.map((item, index) => {
                     return (
-                        <Box sx={{px: "24px", borderInline: index == 1 ? `1px solid ${theme.palette.primary.main}` : "" }}>
-                            <Typography
-                                sx={{...theme.typography.bodyMedium, color: "#3D4D5C80"}}>{item.title}</Typography>
-                            <Typography
-                                sx={{...theme.typography.semiBoldMedium, color: "#6C8093"}}>{item.subTitle}</Typography>
-                        </Box>
+                        <>
+                            {index !== 0 && (
+                                <Divider
+                                    orientation="vertical"
+                                    flexItem
+                                    sx={{borderColor: theme.palette.primary.main, mx: 1}}
+                                />
+                            )}
+                            <Box sx={{px: "24px"}}>
+                                <Typography
+                                    sx={{...theme.typography.bodyMedium, color: "#3D4D5C80"}}>{item.title}</Typography>
+                                <Typography
+                                    sx={{
+                                        ...theme.typography.semiBoldMedium,
+                                        color: "#6C8093"
+                                    }}>{item.subTitle}</Typography>
+                            </Box>
+                        </>
                     )
                 })}
             </Box>
             <Box
-            component={"img"}
-            src={img}
-            sx={{
-                borderRadius: 2,
-                mb: 2
-            }}
+                component={"img"}
+                src={img}
+                sx={{
+                    borderRadius: 2,
+                    mb: 2
+                }}
             >
-
             </Box>
         </Box>
     );

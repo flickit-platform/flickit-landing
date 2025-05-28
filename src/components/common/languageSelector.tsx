c6bd32a88e8e0b10a97799f5b8857acf513868f7
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-05-28 15:12:11 +0330

    FL-23: Fix displayin nav bar

--- src/components/common/languageSelector.tsx
+++ src/components/common/languageSelector.tsx
@@ -1,37 +1,45 @@
 import { useTranslation } from "react-i18next";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import {theme} from "@/config/theme";
+import { theme } from "@/config/theme";
 
 const LanguageSelector = () => {
-    const { i18n } = useTranslation();
+  const { i18n } = useTranslation();
 
-    const handleLanguageChange = (language: string) => {
-        i18n.changeLanguage(language);
-        window.location.reload();
-    };
+  const handleLanguageChange = (language: any) => {
+    i18n.changeLanguage(language);
+    window.location.reload();
+  };
 
-    return (
-        <Box display="flex" justifyContent="center">
-            {i18n.language === 'fa' ? (
-                <Typography
-                    onClick={() => handleLanguageChange('en')}
-                    sx={{cursor: "pointer", fontWeight: "bold", ...theme.typography.titleMedium }}
-                    color="white"
-                >
-                    فا
-                </Typography>
-            ) : (
-                <Typography
-                    onClick={() => handleLanguageChange('fa')}
-                    sx={{cursor: "pointer", fontWeight: "bold", ...theme.typography.titleMedium }}
-                    color="white"
-                >
-                    EN
-                </Typography>
-            )}
-        </Box>
-    );
+  return (
+    <Box display="flex" justifyContent="center">
+      {i18n.language === "fa" ? (
+        <Typography
+          onClick={() => handleLanguageChange("en")}
+          sx={{
+            cursor: "pointer",
+            fontWeight: "bold",
+            ...theme.typography.titleMedium,
+          }}
+          color="white"
+        >
+          فا
+        </Typography>
+      ) : (
+        <Typography
+          onClick={() => handleLanguageChange("fa")}
+          sx={{
+            cursor: "pointer",
+            fontWeight: "bold",
+            ...theme.typography.titleMedium,
+          }}
+          color="white"
+        >
+          EN
+        </Typography>
+      )}
+    </Box>
+  );
 };
 
 export default LanguageSelector;

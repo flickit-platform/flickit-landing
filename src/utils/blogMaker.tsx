2ecdd475378c34af74e0c9db2b8688a972d240d2
e.mostafa <e.mostafa021@gmail.com>
2025-06-01 13:15:17 +0330

    FL-16: fix : comment logic

--- src/utils/blogMaker.tsx
+++ src/utils/blogMaker.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Typography from "@mui/material/Typography";
 import {theme} from "@/config/theme";
 import {styles} from "@/config/styles";
@@ -9,95 +10,94 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Box from "@mui/material/Box";
 
-export const BlogMaker = (item: any) => {
-    const {title, p, ul, table, pBold, topTitle, mainTitle, subTitle, infoBox, image, ulBold} = item
 
-    if (title) {
-        return <Typography component={"h2"}
-                           sx={{...theme.typography.semiBoldXLarge, color: "#000", mb: 1}}>{title}</Typography>
-    } else if (p) {
-        return <Typography sx={{...theme.typography.semiBoldMedium, color: "#2B333B", mb: 2, fontWeight: "regular"}}>{p}</Typography>
-    } else if (topTitle) {
-        return <Typography sx={{...theme.typography.semiBoldXLarge, color: theme.palette.primary.main}}>
-            {topTitle}
+const typeRenderMap: Record<string, (value: any) => JSX.Element> = {
+    title: (value) => (
+        <Typography sx={{...theme.typography.semiBoldXLarge, color: "#000", mb: 1}}>{value}</Typography>
+    ),
+    p: (value) => (
+        <Typography sx={{...theme.typography.semiBoldMedium, color: "#2B333B", mb: 2, fontWeight: "regular"}}>{value}</Typography>
+    ),
+    topTitle: (value) => (
+        <Typography sx={{...theme.typography.semiBoldXLarge, color: theme.palette.primary.main}}>
+            {value}
         </Typography>
-    } else if (mainTitle) {
-        return <Typography sx={{
+    ),
+    mainTitle: (value) => (
+        <Typography sx={{
             ...theme.typography.headlineSmall,
             mt: 2,
             fontSize: {xs: "1.6rem", sm: "3rem"},
             fontWeight: "bold",
             lineHeight: "auto"
         }}>
-            {mainTitle}
+            {value}
         </Typography>
-    } else if (subTitle) {
-        return <Typography sx={{
+    ),
+    subTitle: (value) => (
+        <Typography sx={{
             ...theme.typography.semiBoldMedium,
             color: "#3D4D5C80",
             mt: 2,
         }}>
-            {subTitle}
+            {value}
         </Typography>
-    } else if (ul) {
-        return <List disablePadding sx={{listStyleType: 'disc', paddingInlineStart: 3, mb: 2}}>
-            {ul.map((itemText: any) => {
-                const {li} = itemText
+    ),
+    ul: (value) => (
+        <List disablePadding sx={{listStyleType: 'disc', paddingInlineStart: 3, mb: 2}}>
+            {value.map((itemText: any, i: number) => (
+                <ListItem key={i} sx={{display: 'list-item'}} disablePadding>
+                    <ListItemText primaryTypographyProps={{
+                        sx: {
+                            all: "unset", ...theme.typography.semiBoldMedium,
+                            color: "#2B333B"
+                        }
+                    }}>{itemText.li}</ListItemText>
+                </ListItem>
+            ))}
+        </List>
+    ),
+    ulBold: (value) => (
+        <List disablePadding sx={{listStyleType: 'disc', paddingInlineStart: 3, mb: 2}}>
+            {value.map((itemText: any, index: number) => {
+                const {li} = itemText;
+                const colonIndex = li.indexOf(':');
+                const periodIndex = li.indexOf('.');
+                let splitIndex = -1;
+
+                if (colonIndex !== -1 && (periodIndex === -1 || colonIndex < periodIndex)) {
+                    splitIndex = colonIndex;
+                } else if (periodIndex !== -1) {
+                    splitIndex = periodIndex;
+                }
+
+                const hasSplit = splitIndex !== -1;
+                const boldText = hasSplit ? li.slice(0, splitIndex) : li;
+                const restText = hasSplit ? li.slice(splitIndex) : '';
+
                 return (
-                    <ListItem sx={{display: 'list-item'}} disablePadding>
-                        <ListItemText primaryTypographyProps={{
-                            sx: {
-                                all: "unset", ...theme.typography.semiBoldMedium,
-                                color: "#2B333B"
-                            }
-                        }}>{li}</ListItemText>
+                    <ListItem key={index} sx={{display: 'list-item'}} disablePadding>
+                        <ListItemText
+                            primaryTypographyProps={{
+                                component: "span",
+                                sx: {
+                                    all: "unset",
+                                    ...theme.typography.semiBoldMedium,
+                                    color: "#2B333B",
+                                    fontWeight: "regular"
+                                },
+                            }}
+                        >
+                            <Box component="span" fontWeight="bold">{boldText}</Box>
+                            {restText}
+                        </ListItemText>
                     </ListItem>
-                )
+                );
             })}
         </List>
-    }else if(ulBold){
-      return <List disablePadding sx={{ listStyleType: 'disc', paddingInlineStart: 3, mb: 2 }}>
-          {ulBold.map((itemText: any, index: number) => {
-              const { li } = itemText;
-
-              const colonIndex = li.indexOf(':');
-              const periodIndex = li.indexOf('.');
-              let splitIndex = -1;
-
-              if (colonIndex !== -1 && (periodIndex === -1 || colonIndex < periodIndex)) {
-                  splitIndex = colonIndex;
-              } else if (periodIndex !== -1) {
-                  splitIndex = periodIndex;
-              }
-
-              const hasSplit = splitIndex !== -1;
-              const boldText = hasSplit ? li.slice(0, splitIndex) : li;
-              const restText = hasSplit ? li.slice(splitIndex) : '';
-
-              return (
-                  <ListItem key={index} sx={{ display: 'list-item' }} disablePadding>
-                      <ListItemText
-                          primaryTypographyProps={{
-                              component: "span",
-                              sx: {
-                                  all: "unset",
-                                  ...theme.typography.semiBoldMedium,
-                                  color: "#2B333B",
-                                  fontWeight: "regular"
-                              },
-                          }}
-                      >
-                          <Box component="span" fontWeight="bold">
-                              {boldText}
-                          </Box>
-                          {restText}
-                      </ListItemText>
-                  </ListItem>
-              );
-          })}
-      </List>
-    } else if (table) {
-        const {thead, tbody} = table
+    ),
+    table: (value) => {
+        const {thead, tbody} = value;
         return (
             <TableContainer sx={{mb: 2}} component={Paper}>
                 <Table>
@@ -123,45 +123,57 @@ export const BlogMaker = (item: any) => {
                     </TableBody>
                 </Table>
             </TableContainer>
-        )
-    } else if (pBold) {
-        return <Typography
-            sx={{...theme.typography.semiBoldMedium, color: "#2B333B", mb: 2, fontWeight: "bold"}}>{pBold}</Typography>
-    } else if (infoBox) {
-        return <Box sx={{...styles.centerVH, my: 2, py: 2}}>
-            {infoBox.map((item: any, index: number) => {
-                return (
-                    <>
-                        {index !== 0 && (
-                            <Divider
-                                orientation="vertical"
-                                flexItem
-                                sx={{borderColor: theme.palette.primary.main, mx: 1}}
-                            />
-                        )}
-                        <Box sx={{px: "24px"}}>
-                            <Typography
-                                sx={{...theme.typography.bodyMedium, color: "#3D4D5C80"}}>{item.title}</Typography>
-                            <Typography
-                                sx={{
-                                    ...theme.typography.semiBoldMedium,
-                                    color: "#6C8093"
-                                }}>{item.subTitle}</Typography>
-                        </Box>
-                    </>
-                )
-            })}
+        );
+    },
+    pBold: (value) => (
+        <Typography
+            sx={{...theme.typography.semiBoldMedium, color: "#2B333B", mb: 2, fontWeight: "bold"}}>{value}</Typography>
+    ),
+    infoBox: (value) => (
+        <Box sx={{...styles.centerVH, my: 2, py: 2}}>
+            {value.map((item: any, index: number) => (
+                <React.Fragment key={index}>
+                    {index !== 0 && (
+                        <Divider
+                            orientation="vertical"
+                            flexItem
+                            sx={{borderColor: theme.palette.primary.main, mx: 1}}
+                        />
+                    )}
+                    <Box sx={{px: "24px"}}>
+                        <Typography sx={{...theme.typography.bodyMedium, color: "#3D4D5C80"}}>{item.title}</Typography>
+                        <Typography sx={{...theme.typography.semiBoldMedium, color: "#6C8093"}}>{item.subTitle}</Typography>
+                    </Box>
+                </React.Fragment>
+            ))}
         </Box>
-    } else if (image) {
-        return <Box
+    ),
+    image: (value) => (
+        <Box
             component={"img"}
-            src={image}
+            src={value}
             sx={{
                 borderRadius: 2,
                 mb: 4,
-                width: {xs:  "90%", sm: "100%"}
+                width: {xs: "90%", sm: "100%"}
             }}
-        >
-        </Box>
-    }
+        />
+    ),
 }
+
+export const BlogMaker = (item: any) => {
+    const definedEntries = Object.entries(item).filter(([_, value]) => value !== undefined);
+
+    if (definedEntries.length === 0) return null;
+
+    const [typeKey, value] = definedEntries[0];
+
+    const Renderer = typeRenderMap[typeKey];
+
+    if (Renderer) {
+        return Renderer(value);
+    }
+
+    return null;
+}
+

03179af73a211747d684f0af138aaf98602d7cc5
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-06-01 04:42:31 +0330

    FL-37: Fix responsive bug

--- src/components/homePage/ProcessRoadmap.tsx
+++ src/components/homePage/ProcessRoadmap.tsx
@@ -157,7 +157,7 @@ const RoadmapImageWithPins = ({
     sx={{
       position: "relative",
       width: "100%",
-      maxWidth: isMobile ? "unset" : 1440,
+      maxWidth: isMobile ? 470 : 1440,
       aspectRatio: isMobile ? "442/717" : "1440/831",
       mx: "auto",
       my: isMobile ? 2 : 0,
@@ -231,7 +231,7 @@ const RoadmapImageWithPins = ({
 );
 
 const ProcessRoadmap: React.FC = () => {
-  const isMobile = useScreenResize("sm");
+  const isMobile = useScreenResize("md");
   const streetImg = isMobile ? "/mobile-street.svg" : "/street.svg";
 
   return (

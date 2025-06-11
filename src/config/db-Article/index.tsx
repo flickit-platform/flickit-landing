8497fc2542898ed52e20abc17228aff8c7b052ad
e.mostafa <e.mostafa021@gmail.com>
2025-06-11 18:38:32 +0330

    FL-32: fix : add amazon article

--- src/config/db-Article/index.tsx
+++ src/config/db-Article/index.tsx
@@ -1,4 +1,6 @@
 import facebookFA from "./FaFacebookArticle.json"
 import facebookEN from "./EnFacebookArticle.json"
+import amazonFA from "./FaAmazonArticle.json"
+import amazonEN from "./EnAmazonArticle.json"
 
-export  {facebookFA, facebookEN}
+export  {facebookFA, facebookEN, amazonFA, amazonEN}

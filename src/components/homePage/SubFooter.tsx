3d77e7bd2ef5aee92deaa7719db604d7a741f219
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-06-11 21:07:58 +0330

    FL-32: Fix footer bug

--- src/components/homePage/SubFooter.tsx
+++ src/components/homePage/SubFooter.tsx
@@ -4,7 +4,7 @@ import { Trans, useTranslation } from "react-i18next";
 import Button from "@mui/material/Button";
 import useScreenResize from "@/utils/useScreenResize";
 import React from "react";
-import {theme} from "@/config/theme";
+import { theme } from "@/config/theme";
 
 export default function FooterGradientCTA() {
   const isMobile = useScreenResize("md");
@@ -16,48 +16,57 @@ export default function FooterGradientCTA() {
       sx={{
         width: "100%",
         height: {
-            xs: "155px",
-            sm: "150px",
-            md: "70px",
-            xl: "85px",
+          xs: "155px",
+          sm: "150px",
+          md: "70px",
+          xl: "85px",
         },
         display: "flex",
-        flexDirection: {xs: "column", md: "row"},
+        flexDirection: { xs: "column", md: "row" },
         alignItems: "center",
         justifyContent: "center",
         gap: 3,
         position: "relative",
       }}
     >
-        <Box
+      <Box
         sx={{
-            position: "absolute",
-            width: "100%",
-            height: {
-                xs: "200px",
-                sm: "219px",
-                md: "240px",
-                xl: "245px"
-            },
-            backgroundImage:{
-                xs: "url(/sub-footer-xs.svg)",
-                sm: "url(/sub-footer-sm.svg)",
-                md: theme.direction == "rtl" ? "url(/fa-sub-footer-md.svg)" : "url(/en-sub-footer-md.svg)",
-                xl: theme.direction == "rtl" ? "url(/fa-sub-footer-xl.svg)" : "url(/en-sub-footer-xl.svg)",
-            },
-            backgroundSize: {xs: "cover", md: "contain"},
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            overflow: "visible",
-            filter: 'drop-shadow(0 0 4px #000)',
+          position: "absolute",
+          width: "100%",
+          height: {
+            xs: "200px",
+            sm: "219px",
+            md: "240px",
+            xl: "245px",
+          },
+          backgroundImage: {
+            xs: "url(/sub-footer-xs.svg)",
+            sm: "url(/sub-footer-sm.svg)",
+            md:
+              theme.direction == "rtl"
+                ? "url(/fa-sub-footer-md.svg)"
+                : "url(/en-sub-footer-md.svg)",
+            xl:
+              theme.direction == "rtl"
+                ? "url(/fa-sub-footer-xl.svg)"
+                : "url(/en-sub-footer-xl.svg)",
+          },
+          backgroundSize: { xs: "cover", md: "contain" },
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          overflow: "visible",
+          filter: "drop-shadow(0 0 4px #000)",
         }}
-        />
-      <Typography  sx={{
+      />
+      <Typography
+        sx={{
           ...theme.typography.titleLarge,
           textAlign: "center",
           color: "#fff",
-          zIndex: 2}}>
-        {isMobile && (
+          zIndex: 2,
+        }}
+      >
+        {isMobile &&
           message.split(".").map(
             (sentence, i) =>
               sentence.trim() && (
@@ -66,7 +75,7 @@ export default function FooterGradientCTA() {
                   <br />
                 </React.Fragment>
               )
-          ))}
+          )}
       </Typography>
 
       <Button
@@ -78,13 +87,13 @@ export default function FooterGradientCTA() {
           px: 2.25,
           bgcolor: "#fff",
           color: "#2466A8",
-          position: {md: "absolute"},
+          position: { md: "absolute" },
           [theme.direction == "rtl" ? "left" : "right"]: {
-              md: "4%",
-              xl: "8%",
-              xxl: "15%",
+            md: "4%",
+            xl: "8%",
+            xxl: "15%",
           },
-            height: {xl: "58px"},
+          height: { xl: "42px" },
           boxShadow: "0 2px 12px 0 rgba(36,102,168,0.15)",
           "&:hover": {
             bgcolor: "#F3F7FB",
@@ -94,13 +103,7 @@ export default function FooterGradientCTA() {
           whiteSpace: "nowrap",
         }}
       >
-        <Typography
-            sx={{
-                [theme.breakpoints.down("xl")]: {
-                    ...theme.typography.titleMedium
-                },
-                ...theme.typography.titleLarge,
-        }}>
+        <Typography variant="titleMedium">
           <Trans i18nKey="hero.startSelfAssessment" />
         </Typography>
       </Button>

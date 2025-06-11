f925225829d4613ef8e4a384cefc4cd554bca748
e.mostafa <e.mostafa021@gmail.com>
2025-06-11 14:22:05 +0330

    FL-38: fix : fix i18n-next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-    i18n: {
-        defaultLocale: 'fa',
-        locales: ['en', 'fa'],
-        localeDetection: false,
-        localePath: typeof window === 'undefined'
-            ? require('path').resolve('./public/locales')
-            : '/locales',
-        localeStructure: '{{lng}}/{{ns}}',
-    },
     reloadOnPrerender: process.env.NODE_ENV === 'development',
     ns: ['common', 'home', 'footer', 'navbar'],
     defaultNS: 'common',

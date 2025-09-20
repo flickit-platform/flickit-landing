f5dc140efa5474f1b2f6fcf333c4eea960259fed
mahdasjs@gmail.com <mahdasjs@gmail.com>
2025-09-20 05:06:45 +0330

    FL-73: remove unused locales

--- public/locales/fa/index.ts
+++
@@ -1,6 +0,0 @@
-import common from "./common.json";
-import hero from "./hero.json";
-import main from "./main.json";
-import footer from "./footer.json";
-
-export default { common, hero, main, footer };
